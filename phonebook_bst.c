89c45d6a1e721c090e42661c831bd8a55e55e7b3
rex662624 <zfk.662624@gmail.com>
2018-03-01 23:34:23 +0800

    update some comment

--- phonebook_bst.c
+++ phonebook_bst.c
@@ -4,7 +4,7 @@
 #include "phonebook_bst.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
-int Length(entry *head)
+int Length(entry *head)//用來算list總長度
 {
     int count = 0;
     entry *current = head;
@@ -16,7 +16,9 @@ int Length(entry *head)
 
     return count;
 }
-
+//因為word.txt檔裡面的文件都是已經排序好的 所以用Sorted Linked List to Balanced BST ,
+//而不用去byte-by-byte comparsion
+//length第一次進入function時＝list總長度
 treeNode *BuildBST(entry **headRef, int length)
 {
     if (length <= 0)
@@ -55,7 +57,7 @@ entry *findName(char lastName[], treeNode *root)
 entry *append(char lastName[], entry *e)
 {
     e->pNext = (entry *) malloc(sizeof(entry));
-    e->detail = NULL;
+//    e->detail = NULL;
     e = e -> pNext;
     strcpy(e->lastName, lastName);
     e->pNext = NULL;
